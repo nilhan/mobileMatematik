1f372eef289668db23dc410d59aa3cdeddfd58e7
nilhan <nejat.ilhan@gmail.com>
2013-02-08 20:24:54 +0200

    butonlar konuldu

--- mobileMatematik/gen/com/example/mobilematematik/R.java
+++ mobileMatematik/gen/com/example/mobilematematik/R.java
@@ -24,6 +24,10 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int button_divide=0x7f040006;
+        public static final int button_minus=0x7f040004;
+        public static final int button_multiply=0x7f040005;
+        public static final int button_sum=0x7f040003;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
     }
