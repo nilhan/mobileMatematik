4ce3bbb6fd43c1a70f41d8cc73fbdba159580d33
nilhan <nejat.ilhan@gmail.com>
2013-02-09 19:43:47 +0200

    Yeni buttonlar konuldu.

--- mobileMatematik/gen/com/example/mobilematematik/R.java
+++ mobileMatematik/gen/com/example/mobilematematik/R.java
@@ -14,8 +14,7 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int edit_message=0x7f070000;
-        public static final int menu_settings=0x7f070001;
+        public static final int menu_settings=0x7f070000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -26,9 +25,16 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int button_divide=0x7f040006;
+        public static final int button_easy=0x7f040009;
+        public static final int button_medium=0x7f04000a;
         public static final int button_minus=0x7f040004;
         public static final int button_multiply=0x7f040005;
+        public static final int button_prosesTypeSideBySide=0x7f04000d;
+        public static final int button_prosesTypeTopButtom=0x7f04000e;
+        public static final int button_some_hard=0x7f04000b;
         public static final int button_sum=0x7f040003;
+        public static final int button_veryEasy=0x7f040008;
+        public static final int button_very_hard=0x7f04000c;
         public static final int edit_message=0x7f040007;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
