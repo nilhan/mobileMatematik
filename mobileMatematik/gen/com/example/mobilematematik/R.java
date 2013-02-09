a500f27796d7e086ef98724a2665fe6825287e74
nilhan <nejat.ilhan@gmail.com>
2013-02-09 14:29:11 +0200

    commit

--- mobileMatematik/gen/com/example/mobilematematik/R.java
+++ mobileMatematik/gen/com/example/mobilematematik/R.java
@@ -14,7 +14,8 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int edit_message=0x7f070000;
+        public static final int menu_settings=0x7f070001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -28,6 +29,7 @@ public final class R {
         public static final int button_minus=0x7f040004;
         public static final int button_multiply=0x7f040005;
         public static final int button_sum=0x7f040003;
+        public static final int edit_message=0x7f040007;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
     }
